92a0dc94d6a603c6789565de0e744b4b55dccffb
Kamilo Herrera <kherrera74@uan.edu.co>
2022-05-10 22:44:42 -0500

    Second commit to division

--- Taller/src/Operations.java
+++ Taller/src/Operations.java
@@ -3,12 +3,14 @@
 public class Operations {
 
 	public static void main (String arguments[]) {
+		new Operations().division();		
+	}
+	
+	private void division() {
 		int num1 = 100, num2 = 50, result = 0;
 		System.out.println("dividend " + num1);
 		System.out.println("divider " + num2);
 		result = num1 /  num2;
 		System.out.println("Outcome " + result);
-		
 	}
-	
 }
