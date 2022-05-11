5670b5efafae3a7c9e9e4f7896779224cab5390c
Kamilo Herrera <kherrera74@uan.edu.co>
2022-05-10 22:20:35 -0500

    Creation Class for operations, first commit for division

---
+++ Taller/src/Operations.java
@@ -0,0 +1,14 @@
+
+
+public class Operations {
+
+	public static void main (String arguments[]) {
+		int num1 = 100, num2 = 50, result = 0;
+		System.out.println("dividend " + num1);
+		System.out.println("divider " + num2);
+		result = num1 /  num2;
+		System.out.println("Outcome " + result);
+		
+	}
+	
+}
