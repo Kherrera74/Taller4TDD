c3a46ac5b1cf1c97b1e9c7a20ceffaf23e1f39f3
santy <santycarrillo17@gmail.com>
2022-05-11 20:12:43 -0500

    Estructura codigo

---
+++ Taller/src/Test/TestSuma.java
@@ -0,0 +1,18 @@
+package Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class TestSuma {
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
